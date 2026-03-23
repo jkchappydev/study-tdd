3d435127b048c3919857da6e91601bbc019b9a3a
jkchappydev <jkchappydev@gmail.com>
2026-03-24 01:46:15 +0900

    study: TDD: Test Driven Development - Production Code 보다 Test Code 를 먼저 작성하여 테스트가 구현 과정을 주도하도록 하는 방법론 - TDD 개발 방법론에서는 일반적으로 RED, GREEN, REFACTOR 이라는 세가지 단계 - RED 라고 하는 실패하는 테스트 코드를 먼저 작성 - GREEN 이라고 하는 빠른 시간내에 테스트를 통과하도록 하는 테스트 코드 - REFACTOR 라고 하는, 초록불을 유지하면서 GREEN 에서 아무렇게나 작성한 테스트 코드를 개선하는 작업 TDD 사용이유 - 선 기능 구현, 후 테스트 작성의 한계 	1. 테스트 자체의 누락 가능성 	2. 특정 테스트 케이스 (해피 케이스) 만 검증할 가능성 	3. 잘못된 구현을 다소 늦게 발견할 가능성 - 선 테스트 작성, 후 기능 구현 	1. 복잡도가 낮은, 테스트 가능한 코드로 구현할 수 있게 한다. 	2. 쉽게 발견하기 어려운 엣지(Edge) 케이스를 놓치지 않게 해준다. 	3. 구현에 대한 빠른 피드백을 받을 수 있다. 	4. 과감한 리팩토링이 가능해진다.

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,6 +3,9 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -81,6 +84,59 @@ class CafeKioskTest {
 
         cafeKiosk.clear();
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void calculateTotalPrice() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        assertThat(totalPrice).isEqualTo(8500);
+    }
+
+    @Test
+    void createOrder() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        Order order = cafeKiosk.createOrder();
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    // 해피 케이스
+    @Test
+    void createOrderWithCurrentTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        // 경계값
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2026, 3, 21, 10, 0));
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    // 예외 케이스
+    @Test
+    void createOrderOutsideOpenTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        // 경계값 - 1
+        assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2026, 3, 21, 9, 59)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
     }
 
 }
